afe8c623849411fc808d64bf3444808e0447e46b
Tim Cuthbertson <tim@gfxmonk.net>
2022-08-25 22:36:38 +1000

    it works!

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,8 +6,15 @@ pub fn main() -> Result<()> {
 	env_logger::init();
 	let inject = concat!(env!("CARGO_MANIFEST_DIR"), "/../target/debug/librunix_inject.dylib");
 	debug!("Injecting: {:?}", inject);
-	Err(Command::new("id")
-		.arg("-u")
+	let mut all_args = std::env::args();
+	debug!("{:?}", all_args);
+	let mut args = all_args.into_iter().skip(1);
+	let mut cmd = Command::new(args.next().unwrap());
+	cmd.args(args);
+	info!("{:?}", cmd);
+
+	Err(cmd
 		.env("DYLD_INSERT_LIBRARIES", inject)
+		.env("DYLD_FORCE_FLAT_NAMESPACE", "1")
 		.exec().into())
 }
--- inject/src/lib.rs
+++ inject/src/lib.rs
@@ -1,11 +1,99 @@
-#[macro_use]
-extern crate redhook;
+// mod macros;
+// use crate::macros::*;
 
-use libc::uid_t;
+// #[macro_use]
+// extern crate redhook;
+use redhook::*;
 
-hook! {
-	unsafe fn getuid() -> uid_t => i_am_root {
-		dbg!("getuid() -> 0");
-		0
+use std::borrow::Cow;
+use std::ffi::CStr;
+use std::env;
+use lazy_static::lazy_static;
+
+use libc::{uid_t, c_int};
+
+type opaque_ptr = *const ();
+type string_ptr = *const libc::c_char;
+
+// Emulate C's `__attribute__((constructor))`
+// Under the hood it's just a section with `.init_array` and `.init_array_end` ASM sections.
+#[no_mangle]
+// #[link_section = ".init_array"] // Linux
+#[link_section = "__DATA,__mod_init_func"] // MacOS
+pub static LD_PRELOAD_INITIALISE_RUST: extern "C" fn() = self::ld_preload_init;
+extern "C" fn ld_preload_init() {
+	dbg!("INIT");
+}
+
+
+const REMAP_SRC: &'static [u8] = "/nix/".as_bytes();
+lazy_static! {
+	static ref REMAP: Remap = Remap::from_env();
+}
+
+
+struct Remap {
+	src: &'static [u8],
+	dest: Vec<u8>,
+	diff: usize
+}
+
+impl Remap {
+	pub fn from_env() -> Self {
+		let mut base = env::var("RUNIX_ROOT").unwrap_or_else(|k|panic!("${} required", k)).into_bytes();
+		if !base.ends_with(&['/' as u8]) {
+			base.push('/' as u8);
+		}
+		Self::for_dest(base)
+	}
+	
+	pub fn for_dest<V: Into<Vec<u8>>>(dest: V) -> Self {
+		let dest_bytes = dest.into();
+		let diff: usize = if dest_bytes.len() > REMAP_SRC.len() {
+			dest_bytes.len() - REMAP_SRC.len()
+		} else {
+			panic!("RUNIX_ROOT must be at least {} bytes; got {}", REMAP_SRC.len(), dest_bytes.len());
+		};
+
+		Self {
+			src: REMAP_SRC,
+			dest: dest_bytes,
+			diff
+		}
+	}
+	
+	pub fn intercept<'a>(&self, bytes: &'a [u8]) -> Cow<'a, [u8]> {
+		if bytes.starts_with(self.src) {
+			// dbg!("remapping");
+			// dbg!(String::from_utf8(self.dest.clone()));
+			let suffix = &bytes[self.src.len()..];
+			let mut vec: Vec<u8> = Vec::with_capacity(bytes.len() + self.diff);
+			vec.extend(&self.dest);
+			vec.extend(suffix);
+			Cow::Owned(vec)
+		} else {
+			Cow::Borrowed(bytes)
+		}
 	}
 }
+
+fn intercept<F, R>(s: string_ptr, func: F) -> R where F: FnOnce(string_ptr) -> R {
+	unsafe {
+		let orig = CStr::from_ptr(s).to_bytes();
+		dbg!(String::from_utf8(orig.into()));
+		let cow = REMAP.intercept(orig);
+		dbg!(String::from_utf8(cow.to_vec()));
+		let bytes = cow.as_ref();
+		let cstr = CStr::from_bytes_with_nul_unchecked(bytes);
+		func(cstr.as_ptr())
+	}
+}
+
+hook! { unsafe fn open(path: string_ptr, flags: libc::c_int, mode: libc::mode_t) -> libc::c_int => wrapped_open {
+	intercept(path, |p| open(p, flags, mode))
+}}
+
+hook! { unsafe fn getuid() -> uid_t => wrapped_getuid {
+	dbg!("getuid() -> 0");
+	0
+}}
