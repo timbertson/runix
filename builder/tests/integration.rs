bc870f03a9c67557b11e362ecba1f84fc9268134
Tim Cuthbertson <tim@gfxmonk.net>
2024-02-01 22:14:53 +1100

    clean up test output; use local exe instead of bootstrapping when running a wrapper script

--- builder/tests/integration.rs
+++ builder/tests/integration.rs
@@ -1,72 +1,95 @@
 use anyhow::*;
 use runix_build::run_output;
 use serial_test::serial;
-use std::{process::{Command, Stdio}, path::PathBuf, fs, env};
+use std::{process::{Command}, path::PathBuf, fs, env};
+use std::sync::Once;
 use itertools::Itertools;
 
-fn runix_cmd() -> Result<Command> {
+const BOOTSTRAPPING_LOG: &str = "Note: runix not detected; bootstrapping ...";
+static BUILT_RUNIX_EXE: Once = Once::new();
+
+fn runix_exe() -> Result<PathBuf> {
 	let runix_exe = "../build/runix";
-	assert!(Command::new("gup").arg("-u").arg(runix_exe).spawn()?.wait()?.success());
-	Ok(Command::new(runix_exe))
+	BUILT_RUNIX_EXE.call_once(|| {
+		run(Command::new("gup").arg("-u").arg(runix_exe)).unwrap()
+	});
+	Ok(PathBuf::from(runix_exe).canonicalize()?)
+}
+
+fn runix_cmd() -> Result<Command> {
+	Ok(Command::new(runix_exe()?))
 }
 
 fn run_store_path(pname: &str, args: Vec<&str>) -> Result<String> {
 	let mut path = PathBuf::from("../build/store-paths");
 	path.push(format!("{}.drv", pname));
-	assert!(Command::new("gup").arg("-u").arg(&path).spawn()?.wait()?.success());
+	run(Command::new("gup").arg("-u").arg(&path))?;
 	let store_path = fs::read_to_string(path)?;
 
-	let mut cmd = runix_cmd()?;
-	cmd.env("RUNIX_CHECK", "1");
-	cmd.arg("--require").arg(store_path.trim()).args(args);
-	cmd_output(cmd)
+	cmd_output(
+		runix_cmd()?
+		.env("RUNIX_CHECK", "1")
+		.arg("--require").arg(store_path.trim()).args(args)
+	)
 }
 
-fn cmd_output(mut cmd: Command) -> Result<String> {
-	println!("{:?}", &cmd);
-	let output = cmd.output()?;
+fn cmd_output(cmd: &mut Command) -> Result<String> {
+	println!("+ {:?}", cmd);
+	let output = cmd.output().with_context(||format!("Spawn {:?}", cmd))?;
 	let stderr = String::from_utf8(output.stderr)?;
 	let stdout = String::from_utf8(output.stdout)?;
 	let all_lines =
 		stderr.lines().map(|l| format!("[stdout] {}", l)).chain(
 			stdout.lines().map(|l| format!("[stderr] {}", l))
 		);
+
 	#[allow(unstable_name_collisions)]
-	Ok(all_lines.intersperse("\n".to_owned()).collect())
+	let all_output = all_lines.intersperse("\n".to_owned()).collect();
+	if !output.status.success() {
+		println!("{}", all_output);
+		Err(anyhow!("Command failed: {:?}", cmd))
+	} else {
+		Ok(all_output)
+	}
 }
 
 fn run(cmd: &mut Command) -> Result<()> {
-	println!("{:?}", &cmd);
-	let result = cmd.spawn().with_context(||format!("Spawn {:?}", &cmd))?.wait()?;
-	if result.success() {
-		Ok(())
-	} else {
-		Err(anyhow!("Comand failed: {:?}", cmd))
-	}
+	let _ = cmd_output(cmd)?;
+	Ok(())
 }
 
 fn build_wrapper(pname: &str) -> PathBuf {
 	let mut path = PathBuf::from("../build/wrappers");
 	path.push(pname);
-	assert!(Command::new("gup").arg("-u").arg(&path).spawn().unwrap().wait().unwrap().success());
+	run(Command::new("gup").arg("-u").arg(&path)).unwrap();
 	path
 }
 
 fn run_wrapper(pname: &str, args: Vec<&str>) -> Result<String> {
 	let path = build_wrapper(pname);
-	
-	let mut cmd = Command::new(&path);
-	cmd.args(args.clone());
-	let output = cmd_output(cmd)?;
+	test_in_temp_runix(|tmp| {
+		// skip bootstrapping check
+		let runix_path_in_tmp_root = PathBuf::from(tmp).join("current/bin/runix");
+		fs::create_dir_all(runix_path_in_tmp_root.parent().unwrap())?;
+		std::os::unix::fs::symlink(runix_exe()?, runix_path_in_tmp_root)
+			.context("symlinking runix_exe")?;
 
-	// aside: assert that all variants also succeed
-	for alt_path in vec!(path.with_extension("multiplatform-attrs"), path.with_extension("multiplatform")) {
-		let mut cmd = Command::new(alt_path);
-		cmd.args(args.clone());
-		cmd.stdout(Stdio::null());
-		assert_eq!(Some(0), cmd.status()?.code());
-	}
-	Ok(output)
+		let output = cmd_output(
+			Command::new(&path)
+			.args(args.clone())
+			.env("RUNIX_ROOT", tmp)
+		)?;
+
+		// aside: assert that all variants also succeed
+		for alt_path in vec!(path.with_extension("multiplatform-attrs"), path.with_extension("multiplatform")) {
+			run(
+				Command::new(alt_path)
+				.args(args.clone())
+				.env("RUNIX_ROOT", tmp)
+			)?;
+		}
+		Ok(output)
+	})
 }
 
 fn assert_contains<S: AsRef<str>>(needle: &'static str, data: S) -> Result<()> {
@@ -78,9 +101,18 @@ fn assert_contains<S: AsRef<str>>(needle: &'static str, data: S) -> Result<()> {
 	}
 }
 
+fn assert_not_contains<S: AsRef<str>>(needle: &'static str, data: S) -> Result<()> {
+	let data_ref = data.as_ref();
+	if data_ref.contains(needle) {
+		Err(anyhow!("Value contains '{}':\n\n{}", needle, data_ref))
+	} else {
+		Ok(())
+	}
+}
+
 #[test]
 #[serial]
-fn gnupg() -> Result<()> {
+fn test_gnupg() -> Result<()> {
 	let output = run_store_path("gnupg", vec!("gpg", "--help"))?;
 	assert_contains("gpg (GnuPG)", output)
 }
@@ -89,9 +121,10 @@ const JQ_HELP_TEXT: &str = "jq - commandline JSON processor";
 
 #[test]
 #[serial]
-fn jq() -> Result<()> {
+fn test_jq_wrapper() -> Result<()> {
 	let output = run_wrapper("jq", vec!("--help"))?;
-	assert_contains(JQ_HELP_TEXT, output)
+	assert_not_contains(BOOTSTRAPPING_LOG, &output)?;
+	assert_contains(JQ_HELP_TEXT, &output)
 }
 
 #[test]
@@ -114,15 +147,18 @@ fn cleanup_runix_root(tmp: &str) -> Result<()> {
 	}
 }
 
-fn test_in_temp_runix<F: FnOnce(&str) -> Result<()>>(f: F) -> Result<()> {
+fn test_in_temp_runix<T, F: FnOnce(&str) -> Result<T>>(f: F) -> Result<T> {
 	let tmp = "/tmp/runix-tests";
 	cleanup_runix_root(tmp)?;
-	let result = f(tmp).and_then(|_| {
-		let mut cmd = Command::new(format!("{}/current/bin/runix", tmp));
-		cmd.arg("--help");
-		cmd.env("RUNIX_ROOT", tmp);
-		cmd_output(cmd)
-	}).and_then(|output| assert_contains("runix RUNSCRIPT [...ARGS]", output));
+	let result = f(tmp).and_then(|result| {
+		cmd_output(
+			Command::new(format!("{}/current/bin/runix", tmp))
+				.arg("--help")
+				.env("RUNIX_ROOT", tmp)
+		)
+			.and_then(|output| assert_contains("runix RUNSCRIPT [...ARGS]", output))
+			.and(Ok(result))
+	});
 	cleanup_runix_root(tmp)?;
 	result
 }
@@ -173,12 +209,11 @@ fn local_auto_bootstrap() -> Result<()> {
 	jq_wrapper.set_extension("bootstrap");
 
 	test_in_temp_runix(|root| {
-		let mut cmd = Command::new(jq_wrapper);
-		cmd.arg("--help")
+		let output = cmd_output(Command::new(jq_wrapper)
+			.arg("--help")
 			.env("RUNIX_ROOT", root)
-			.env("LOCAL_BOOTSTRAP", platform_build);
-		let output = cmd_output(cmd).context("wrapper script")?;
-		assert_contains("Note: runix not detected; bootstrapping ...", &output)?;
+			.env("LOCAL_BOOTSTRAP", platform_build)).context("wrapper script")?;
+		assert_contains(BOOTSTRAPPING_LOG, &output)?;
 		assert_contains(JQ_HELP_TEXT, &output)?;
 		Ok(())
 	})
@@ -202,12 +237,12 @@ fn crossplatform_file_types() -> Result<()> {
 		.arg(format!("{}/all/bootstrap.dir", &platforms))
 	)?;
 	for platform in all_platforms() {
-		let mut find_cmd = Command::new("find");
-		find_cmd.arg(format!("{}/{}/bootstrap.dir", platforms, platform))
+		for full_line in cmd_output(Command::new("find")
+			.arg(format!("{}/{}/bootstrap.dir", platforms, platform))
 			.arg("-type").arg("f")
 			.arg("-executable")
-			.arg("-exec").arg("file").arg("{}").arg(";");
-		for full_line in cmd_output(find_cmd)?.lines() {
+			.arg("-exec").arg("file").arg("{}").arg(";"))?.lines()
+		{
 			let line = full_line.rsplit(": ").next().unwrap();
 			if line.trim().is_empty() {
 				continue
